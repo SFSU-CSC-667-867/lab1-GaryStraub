185af5a924d4f88b3c6f31bed17368097eefde7c
GaryStraub <gstraub@sfsu.edu>
2019-02-06 20:24:34 -0800

    Update functions.js

--- node/functions.js
+++ node/functions.js
@@ -15,5 +15,13 @@ arrow('hi')
 // for practice
 
 // make an arrow function with no block body
+const myFunction = (x) => x + 1;
+console.log(myFunction(4));
 
 // make a function that returns a function
+const functionFactory = () => () => console.log('this is weird');
+const a = functionFactory();
+a();
+
+const anotherFunction = functionFactory;
+anotherFunction()();
