f99e946d18f55bd0f516b0779986ddf267f042e7
GaryStraub <gstraub@sfsu.edu>
2019-02-06 20:40:01 -0800

    Updated Arrays
    Updating Arrays

--- node/arrays.js
+++ node/arrays.js
@@ -15,15 +15,25 @@ array[1] = 'hello';
 // for practice
 
 // use forEach
-
+array.forEach(function(x){
+    console.log(x);
+});
 // use map
+console.log(array.map(x => x*2));
+console.log(array);
+const newArray = array.map(x => x *2);
+console.log(newArray);
 
 // use pop
-
+console.log(array.pop())
+console.log(array);
 // use push
-
+console.log(array.push(7))
+console.log(array);
 // use shift
-
+console.log(array.shift())
+console.log(array);
 // use unshift
-
+array.unshift(43)
+console.log(array);
 // use filter
